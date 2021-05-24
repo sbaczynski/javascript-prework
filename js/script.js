9930bcbd152b6a4482acde5bf8b06f2e611e8c4c
Szymon Baczynski <sbaczynski18@gmail.com>
2021-05-25 00:07:48 +0200

    Define moves

--- js/script.js
+++ js/script.js
@@ -2,10 +2,33 @@ function printMessage(msg){
 	let div = document.createElement('div');
 	div.innerHTML = msg;
 	document.getElementById('messages').appendChild(div);
-    let computerMove = 'kamień';
-    let playerMove = 'papier';
-    printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-    printMessage('Zagrałem' + playerMove + '! Wygrałem :)');
+    let randomNumber = Math.floor(Math.random() * 3 + 1);
+    console.log('Wylosowana liczba to: ' + randomNumber);
+    let computerMove = 'nieznany ruch';
+    if(randomNumber == 1) {
+        computerMove = 'kamień';
+    }
+    else if(randomNumber == 2) {
+        computerMove = 'papier';
+    }
+    else if(randomNumber == 3) {
+        computerMove = 'nozyce';
+    }
+    printMessage('Mój ruch to: ' + computerMove + '!');
+    
+    let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nozyce');
+    console.log('Gracz wpisał: ' + playerInput);
+    let playerMove = 'nieznany ruch';
+    if(playerInput == '1') {
+        playerMove = 'kamień';
+    }
+    else if(playerInput == '2') {
+        playerMove = 'papier';
+    }
+    else if(playerInput == '3') {
+        playerMove = 'nozyce';
+    }
+    printMessage('Zagrałem' + playerMove + '!');
 }
 
 function clearMessages(){
