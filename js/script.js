d1a182547eb4819d5e03723a492d3073011ec9cf
Szymon Baczynski <sbaczynski18@gmail.com>
2021-05-24 23:30:41 +0200

    Add variables

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@ function printMessage(msg){
 	let div = document.createElement('div');
 	div.innerHTML = msg;
 	document.getElementById('messages').appendChild(div);
+    let computerMove = 'kamień';
+    let playerMove = 'papier';
+    printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+    printMessage('Zagrałem' + playerMove + '! Wygrałem :)');
 }
 
 function clearMessages(){
